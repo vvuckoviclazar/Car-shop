ba8f5ce0901275327846d8e5ab6f92105ba76db3
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-02-12 19:34:26 +0100

    Almost finished...

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const carList = document.querySelector(".car-list");
 const availabilitySelect = document.querySelector(".availability");
 const sortSelect = document.querySelector(".options");
 
+let availability;
+let sortOption;
+
+availabilitySelect.addEventListener("change", (e) => {
+  availability = availabilitySelect.value;
+});
+
+sortSelect.addEventListener("change", (e) => {
+  sortOption = sortSelect.value;
+});
+
 const cars = [
   {
     id: 1,
@@ -107,6 +118,77 @@ const cars = [
   },
 ];
 
+function carCreator() {
+  const getName = (car) => {
+    return car.name;
+  };
+
+  const getBrand = (car) => {
+    return car.brand;
+  };
+
+  const getYear = (car) => {
+    return car.manufacturedYear;
+  };
+
+  const getDoors = (car) => {
+    return car.doors;
+  };
+
+  const getImage = (car) => {
+    return car.image;
+  };
+
+  const getId = (car) => {
+    return car.id;
+  };
+
+  const getPrice = (car) => {
+    return car.price;
+  };
+
+  const isAvailable = (car) => {
+    return car.available;
+  };
+
+  return {
+    getName,
+    getBrand,
+    getDoors,
+    getYear,
+    getImage,
+    getId,
+    getPrice,
+    isAvailable,
+  };
+}
+
+const creator = carCreator();
+
+function carManager() {
+  let filteredCars = [...cars];
+
+  const getCars = () => {
+    return filteredCars;
+  };
+
+  const setCars = (newCars) => {
+    filteredCars = newCars;
+  };
+
+  const resetCars = () => {
+    filteredCars = [...cars];
+  };
+
+  const deleteCar = (id) => {
+    setCars(manager.getCars().filter((car) => creator.getId(car) !== id));
+  };
+
+  return { getCars, setCars, resetCars, deleteCar };
+}
+
+const manager = carManager();
+
 function displayCars(filteredCars) {
   carList.innerHTML = "";
 
@@ -115,23 +197,78 @@ function displayCars(filteredCars) {
     carCard.classList.add("car-card");
 
     carCard.innerHTML = `
-     <h3>${car.name}</h3>
+     <h3>${creator.getName(car)}</h3>
      <div class="li-div">
-      <img src="${car.image}" class="car-image-${car.id} car-img">
+      <img src="${creator.getImage(car)}" class="car-img" 
+      id="${creator.getId(car)}">
         <div class="info-div">
-           <p><strong>Brand:</strong> ${car.brand}</p>
-            <p><strong>Year:</strong> ${car.manufacturedYear}</p>
-            <p><strong>Doors:</strong> ${car.doors}</p>
+           <p><strong>Brand:</strong> ${creator.getBrand(car)}</p>
+            <p><strong>Year:</strong> ${creator.getYear(car)}</p>
+            <p><strong>Doors:</strong> ${creator.getDoors(car)}</p>
             <div class="price-div">
              <p class="price-p"><strong>Price:</strong></p>
-             <h4>$${car.price}</h4> 
+             <h4>$${creator.getPrice(car)}</h4> 
             </div>
         </div>
-        <span><strong>Available:</strong> ${car.available}</span>
+        <span><strong>Available:</strong> ${creator.isAvailable(car)}</span>
         <button class="delete-btn">Delete</button>
      </div>
     `;
 
+    const span = carCard.querySelector("span");
+
+    if (creator.isAvailable(car) === "Yes") {
+      span.style.backgroundColor = "rgb(54, 187, 54)";
+    } else {
+      span.style.backgroundColor = "rgb(217, 48, 48)";
+    }
+
+    const deleteBtn = carCard.querySelector(".delete-btn");
+
+    deleteBtn.addEventListener("click", () => {
+      manager.deleteCar(creator.getId(car));
+      carCard.remove();
+      displayCars(manager.getCars());
+    });
+
     carList.appendChild(carCard);
   });
 }
+
+function updateCarList() {
+  manager.resetCars();
+
+  let carsData = manager.getCars();
+
+  if (availability === "available") {
+    carsData = carsData.filter((car) => creator.isAvailable(car) === "Yes");
+  } else if (availability === "availableNot") {
+    carsData = carsData.filter((car) => creator.isAvailable(car) === "No");
+  }
+
+  if (sortOption === "AZ") {
+    carsData.sort((carA, carB) =>
+      creator.getName(carA).localeCompare(creator.getName(carB))
+    );
+  } else if (sortOption === "ZA") {
+    carsData.sort((carA, carB) =>
+      creator.getName(carB).localeCompare(creator.getName(carA))
+    );
+  } else if (sortOption === "priceLowest") {
+    carsData.sort(
+      (carA, carB) => creator.getPrice(carA) - creator.getPrice(carB)
+    );
+  } else if (sortOption === "priceHighest") {
+    carsData.sort(
+      (carA, carB) => creator.getPrice(carB) - creator.getPrice(carA)
+    );
+  }
+
+  manager.setCars(carsData);
+  displayCars(manager.getCars());
+}
+
+availabilitySelect.addEventListener("change", updateCarList);
+sortSelect.addEventListener("change", updateCarList);
+
+displayCars(manager.getCars());
